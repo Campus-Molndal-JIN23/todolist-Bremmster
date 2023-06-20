37d00c18c96887462eb8652ef06313a221a7d2ac
Kristian Karlson <sppdc1@gmail.com>
2023-06-20 12:10:52 +0200

    getters and setters

--- TDDproject/src/main/java/Todo.java
+++ TDDproject/src/main/java/Todo.java
@@ -11,4 +11,36 @@ public class Todo {
         this.done = done;
         this.assignedTo = assignedTo;
     }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getDone() {
+        return this.done;
+    }
+
+    public void setDone(int done) {
+        this.done = done;
+    }
+
+    public int getAssignedTo() {
+        return this.assignedTo;
+    }
+
+    public void setAssignedTo(int assignedTo) {
+        this.assignedTo = assignedTo;
+    }
 }
