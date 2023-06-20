7cdc3940a69e08bf6aeb1c4bd429552302d68de7
Kristian Karlson <sppdc1@gmail.com>
2023-06-20 09:43:20 +0200

    classes created

---
+++ TDDproject/src/main/java/Todo.java
@@ -0,0 +1,7 @@
+public class Todo {
+
+    private int id;
+    private String text;
+    private boolean done;
+    private int assignedTo;
+}
---
+++ TDDproject/src/main/java/User.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class User {
+
+    private int id;
+    private String name;
+    private int age;
+    private List<Todo> todos;
+}
