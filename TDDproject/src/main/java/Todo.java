532b62104fe33a1ac4af473c06ea45e1c65e89a6
Kristian Karlson <sppdc1@gmail.com>
2023-06-20 11:40:16 +0200

    added constructor

--- TDDproject/src/main/java/Todo.java
+++ TDDproject/src/main/java/Todo.java
@@ -2,6 +2,13 @@ public class Todo {
 
     private int id;
     private String text;
-    private boolean done;
+    private int done;
     private int assignedTo;
+
+    public Todo(int id, String text, int done, int assignedTo) {
+        this.id = id;
+        this.text = text;
+        this.done = done;
+        this.assignedTo = assignedTo;
+    }
 }
