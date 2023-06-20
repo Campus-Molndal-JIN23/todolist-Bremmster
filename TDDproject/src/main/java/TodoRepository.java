0aa296daa677e04955ccd8894f210ca896011896
Kristian Karlson <sppdc1@gmail.com>
2023-06-20 11:41:31 +0200

    added posibility to search by id. and list all todos

--- TDDproject/src/main/java/TodoRepository.java
+++ TDDproject/src/main/java/TodoRepository.java
@@ -1,2 +1,56 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoRepository {
+    private final String dbName;
+    private Connection connection;
+
+    public TodoRepository() throws SQLException {
+        this.dbName = "todo_app_db";
+        connection = DriverManager.getConnection("jdbc:sqlite:" + dbName + ".db");
+    }
+
+    public Todo getTodoById(int id) throws SQLException {
+
+        String sql = "SELECT * FROM " + this.dbName + " WHERE id LIKE ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, "%" + id + "%");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        return new Todo(resultSet.getInt("id"),
+                resultSet.getString("text"),
+                resultSet.getInt("done"),
+                resultSet.getInt("assignedTo")
+        );
+    }
+
+    public List<Todo> getAllTodos() throws SQLException {
+
+        String sql = "SELECT * FROM " + this.dbName + " WHERE id LIKE ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, "%" + "*" + "%");
+
+        return getTodos(preparedStatement);
+    }
+
+    private List<Todo> getTodos(PreparedStatement preparedStatement) throws SQLException { // KK
+        // Used by all the sql query, adds result to list. If nothing is found return null
+        List<Todo> todos = new ArrayList<>();
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            todos.add(new Todo(resultSet.getInt("id"),
+                    resultSet.getString("text"),
+                    resultSet.getInt("done"),
+                    resultSet.getInt("asignedTo")
+            ));
+        }
+        if (todos.isEmpty()) {
+            return null;
+        }
+        return todos;
+    }
+
 }
