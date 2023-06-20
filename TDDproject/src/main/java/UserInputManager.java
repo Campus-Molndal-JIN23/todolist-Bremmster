649c77502d7221280ed8d03771239d00c301a45e
Kristian Karlson <sppdc1@gmail.com>
2023-06-20 15:51:11 +0200

    made static

--- TDDproject/src/main/java/UserInputManager.java
+++ TDDproject/src/main/java/UserInputManager.java
@@ -3,23 +3,17 @@ import java.util.Scanner;
 
 public class UserInputManager {
 
-    private static Scanner scanner;
+    private static Scanner scanner = new Scanner(System.in);
 
-    public UserInputManager() {
-        this.scanner = new Scanner(System.in);
-    }
-
-    public String getString() {
-        return scanner.nextLine();
-        // todo try catch
-    }
 
     public static int menuChoice(int min, int max) {
+        // todo remove static?
         int usrChoice;
 
         while (true) {
             try {
                 usrChoice = Integer.parseInt(scanner.nextLine());
+                // todo check i this works
                 if (usrChoice >= min && usrChoice <= max || usrChoice == 9) break;
             } catch (InputMismatchException e) {
                 System.out.print("Not an Integer\nPleaser enter Integer: ");
@@ -27,5 +21,10 @@ public class UserInputManager {
             }
         }
         return usrChoice;
+    }
+
+    public String getString() {
+        return scanner.nextLine();
+        // todo try catch
     }
 }
