dece11f92bca22099a0b16fdb830495af3b0d1ce
Kristian Karlson <sppdc1@gmail.com>
2023-06-22 16:28:51 +0200

    implementing user features

--- src/main/java/userDatabase.java
+++ src/main/java/userDatabase.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 public class userDatabase {
 
@@ -30,5 +31,30 @@ public class userDatabase {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void updateUser(User user) {
+
+        String sql = "UPDATE user SET name = ?, age = ?, WHERE (id = ?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(3, user.getId());
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setInt(2, user.getAge());
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public List<User> getAllUsers() {
+    }
+
+    public User readUserByIndex(int index) {
+    }
+
+    public User readUserName(User user) {
     }
 }
